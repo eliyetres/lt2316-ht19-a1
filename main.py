1df9ec0e8ebaf3e3ffbddfeb582af0152d433c96
eliyetres <eliyetres@hotmail.com>
2019-09-11 16:54:01 +0200

    split files into main and read, added functions for reading data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from read import read_labels, open_datafiles
 # Ignore everything past the first 100 characters of each sentence and remove any instances that have less than 100 characters. 
 
 
@@ -9,7 +10,14 @@
 
 # Choose any 10 languages and document them in your readme. Split out those languages into smaller training and test files and submit them with your project.
 
-# Swedish Danish Norwegian Icelandic Faroese
-# Old-English English Welsh Cornish
+dirr = dirr = "data/" # Data directory
 
-language_labels = ["Swedish", "Danish", "Norwegian", "Icelandic", "Faroese", "Old english", "Welsh", "Cornish", "Breton", "Cornish"]
+languages = ["Swedish", "Danish", "Bokmål", "Icelandic", "Faroese", "English", "Welsh", "Cornish", "Breton", "Cornish"] # Selected languages
+
+lan = read_labels(dirr, "labels.csv", languages) # Language codes
+
+
+
+x_train, y_train = open_datafiles(dirr, "y_train.txt", "x_train.txt",lan)
+
+x_test, y_test = open_datafiles(dirr, "y_test.txt", "x_test.txt",lan)
---
+++ read.py
@@ -0,0 +1,67 @@
+import csv
+import re
+#from main import languages
+
+#dirr = "data/"
+
+#languages = ["Swedish", "Danish", "Bokmål", "Icelandic", "Faroese", "English", "Welsh", "Cornish", "Breton", "Cornish"]
+
+
+
+
+
+def read_labels(file_dir, filename, selected_languages):
+    """
+    Gets the language labels from the selected labels (file type csv)
+    """
+    selected_codes = [] # Selected language codes
+    with open(file_dir + filename, encoding='utf-8') as file_labels:
+        csv_reader = csv.reader(file_labels, delimiter=';')
+        next(csv_reader) # Skip first row
+
+        #print(("{:<25s} {:>10s}").format("Language", "Code"))
+        #print("--------------------------------")
+
+        for row in csv_reader:
+            lang = row[1]
+            code = row[0]
+            #print(("{:<25s} {:>10s}").format(lang, code))
+
+            if lang in selected_languages:
+                selected_codes.append(code)
+        print("Selected language codes: ", selected_codes)
+    return selected_codes
+
+
+
+def open_datafiles(file_dir, lang_labels, lang_data, selected_codes):
+    """ Opens and reads the labels and data """
+    x_data = [] 
+    y_data =[] 
+    # Read labels
+    with open(file_dir + lang_labels, encoding='utf-8') as labels:
+        with open(file_dir + lang_data, encoding='utf-8') as data:
+            for label_line, data_line in zip(labels, data):
+                
+                label_line = re.sub(r'(\n)', '', label_line) # Remove newlines
+                data_line = re.sub(r'(\n)', '', data_line)
+                if label_line in selected_codes: # Get selected languages
+
+                    y = label_line
+                    x = data_line
+
+                    x_data.append(x)
+                    y_data.append(y)
+
+    #for y,x in  zip( y_train[:10], x_train[:10]):
+        #print(y, "::::", x)
+
+    return x_data, y_data
+
+
+
+#read_labels(dirr, "labels.csv", languages)
+#x_train, y_train = open_datafiles(dirr, "y_train.txt", "x_train.txt")
+#x_test, y_test = open_datafiles(dirr, "y_test.txt", "x_test.txt")
+
+
