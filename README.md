8fcb25350579d2c87172fd3f9c905b70f840e9c0
eliyetres <eliyetres@hotmail.com>
2019-09-11 11:25:47 +0200

    language data

---
+++ README.md
@@ -0,0 +1,3 @@
+# LT2316 Machine Learning GU 2019
+
+## Assignment #1: language identification with as few characters as possible
---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# Ignore everything past the first 100 characters of each sentence and remove any instances that have less than 100 characters. 
+
+
+# Requirements: 
+# Implement the learning algorithm in PyTorch.
+
+# Use a recurrent neural network variant, such as a Gated Recurrent Unit (GRU) network, be central to the classifiers design. (You will also probably need an output softmax activation layer.)
+
+
+# Choose any 10 languages and document them in your readme. Split out those languages into smaller training and test files and submit them with your project.
+
+# Swedish Danish Norwegian Icelandic Faroese
+# Old-English English Welsh Cornish
+
+language_labels = ["Swedish", "Danish", "Norwegian", "Icelandic", "Faroese", "Old english", "Welsh", "Cornish", "Breton", "Cornish"]
