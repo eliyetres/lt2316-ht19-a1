5ed6e1960d193d7e6700adc692b10c101d09db55
eliyetres <eliyetres@hotmail.com>
2019-09-25 20:52:35 +0200

    started vectorization

--- vectorization.py
+++ vectorization.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 from nltk.tokenize import WordPunctTokenizer
 
-def load_data(y_datafile,x_datafile):
+def load_data(x_datafile, y_datafile):
     Y = []
     X = []
     with open(y_datafile, encoding='utf-8') as labels:
@@ -10,4 +10,16 @@ def load_data(y_datafile,x_datafile):
             for label_line, data_line in zip(labels, data):
                 Y.append(label_line)
                 X.append(data_line)
-    return Y, X
+    return X, Y
+
+X, Y = load_data("x.txt","y.txt")
+
+def vectorize(X,Y):
+    for label, data in zip(Y,X):
+        data = list(data) # split on character labels
+        print(data[:50])
+
+
+
+
+vectorize(X,Y)
